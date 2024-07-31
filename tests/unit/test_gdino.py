7a3639fefae4b4c12edec3b1431468217f3a9fa5
Rhys Williams <rhys@pronti.ai>
2024-07-31 00:59:37 -0400

    correcting instantiation unit test path

--- tests/unit/test_gdino.py
+++ tests/unit/test_gdino.py
@@ -6,7 +6,7 @@ from gdino.model import OnnxGDINO
 
 def test_gdino_instantiation():
     onnx_model = OnnxGDINO()
-    assert os.path.isfile('./gdino/data/clip_image_model_vitb32.onnx')
+    assert os.path.isfile('./gdino/data/gdino_fp32.onnx')
 
 
 
